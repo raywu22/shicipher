f2edca32cdfb5b0fa110b5b6c1c55c84c37c0fdb
Raymond Wu <raywu22@mit.edu>
2015-05-25 18:12:34 -0400

    did more stuffs

--- public/javascripts/indexScripts.js
+++ public/javascripts/indexScripts.js
@@ -10,23 +10,46 @@ $(document).ready(function(){
     $('#message-pills ul li').click( function() {
         $(this).addClass('active').siblings().removeClass('active');
         $("#message-button").text($('#message-pills ul .active').text());
+        $("#selection-container .hidden").removeClass('hidden');
+
+        if ($("#message-button").text()==="Encode"){
+            $("#key-container").addClass('hidden');
+            $("#calculated-key-container").removeClass('hidden');
+            $("#key-label").removeClass('hidden');
+        }
+        else {
+            $("#which-selection-container").addClass('hidden');
+            $("#calculated-key-container").addClass('hidden');
+            $("#key-label").addClass('hidden');
+        }
       });
+
+    $("#selection-container ul li").click(function(){
+        $("#dropdownMenu1").text($(this).text());
+    })
 
     $("#message-form").on("submit", function(e) {
         messageValue = $("#message-text").val();
         encodeOrDecode = $('#message-pills ul .active').text();
+        typeEncode = $("#dropdownMenu1").text();
+        keyCode = $("#key-text").val();
         e.preventDefault();
         $.ajax({
             url: $(this).attr("action"),
             type: 'POST',
             data: {
                         "message-text":messageValue,
-                        "typeCode":encodeOrDecode
+                        "typeCode":encodeOrDecode,
+                        "typeEncode":typeEncode,
+                        "keyCode":keyCode
                     },
             beforeSend: function() {
             },
             success: function(message) {
-                $("#returned-text").text(message);
+                console.log(message);
+                $("#returned-text").text(message["encryptedText"]);
+                $("#calculated-key-text").text(message["key"]);
+                $("#key-text").val("");
             }
         });
     });
